b0c4a81399ae10a2dc9a7bc123b5633e4f509193
ashelake <ashelake88@gmail.com>
2022-09-29 21:28:48 +0530

    initial commit

--- src/routes/Payment.jsx
+++ src/routes/Payment.jsx
@@ -3,27 +3,10 @@ import { Subtotal } from "../components/Subtotal";
 
 const Payment = () => {
   return (
-    <Stack
-      border="1px solid"
-      height="500px"
-      display="flex"
-      flexDirection="row"
-      padding="1px"
-    >
-      <Container
-        border="1px solid"
-        height="500px"
-        width="49%"
-        bg="black"
-      ></Container>
-      <Container
-        border="1px solid"
-        height="500px"
-        width="49%"
-        bg="red"
-        marginTop="0"
-      ></Container>
-    </Stack>
+    <div>
+      <div></div>
+      <div></div>
+    </div>
   );
 };
 export { Payment };
